902b977831b091a2d16f543a8daa90a26fa8741d
Alexandr Sugak <alexandr.sugak@gmail.com>
2019-09-22 21:01:16 +0300

    new structure

---
+++ gulpfile.js
@@ -0,0 +1,90 @@
+const gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    babel = require('gulp-babel'),
+    autoprefixer = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync').create(),
+    clean = require('gulp-clean'),
+    rigger = require('gulp-rigger');
+
+const path = {
+    dist: {
+        html: 'dist',
+        css: 'dist/css',
+        js: 'dist/js',
+        img: 'dist/img',
+        self: 'dist'
+    },
+    src : {
+        html: 'src/*.html',
+        htmlWatch: 'src/**/*.html',
+        scss: 'src/scss/*.scss',
+        scssWatch: 'src/scss/**/*.scss',
+        js: 'src/js/*.js',
+        img: 'src/img/**/*.*'
+    }
+};
+
+/************ F U N C T I O N S **************/
+
+const htmlBuild = () => (
+    gulp.src(path.src.html)
+        .pipe(rigger())
+        .pipe(gulp.dest(path.dist.html))
+        .pipe(browserSync.stream())
+);
+
+const imgBuild = () => (
+    gulp.src(path.src.img)
+        .pipe(gulp.dest(path.dist.img))
+        .pipe(browserSync.stream())
+);
+
+const scssBuild = () => (
+    gulp.src(path.src.scss)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer(['> 0.01%', 'last 100 versions']))
+        .pipe(gulp.dest(path.dist.css))
+        .pipe(browserSync.stream())
+);
+
+const jsBuild = () => (
+    gulp.src(path.src.js)
+        .pipe(concat('script.js'))
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(gulp.dest(path.dist.js))
+        .pipe(browserSync.stream())
+);
+
+const cleanDist = () => (
+    gulp.src(path.dist.self, {allowEmpty: true})
+        .pipe(clean())
+);
+
+/************ W A T C H E R **************/
+
+const watcher = () => {
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    });
+    gulp.watch(path.src.htmlWatch, htmlBuild).on('change', browserSync.reload);
+    gulp.watch(path.src.scssWatch, scssBuild).on('change', browserSync.reload);
+    gulp.watch(path.src.js, jsBuild).on('change', browserSync.reload)
+};
+
+/************ T A S K S **************/
+
+gulp.task('html', htmlBuild);
+gulp.task('scss', scssBuild);
+gulp.task('js', jsBuild);
+gulp.task('img', imgBuild);
+
+gulp.task('default', gulp.series(
+    cleanDist,
+    gulp.parallel(htmlBuild, imgBuild, scssBuild, jsBuild),
+    watcher
+));
