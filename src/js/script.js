119d920f72313e11fdb931cbb4423a294c77ed66
Alexandr Sugak <alexandr.sugak@gmail.com>
2019-09-26 20:11:59 +0300

    minor fixes and new structure html

--- src/js/script.js
+++ src/js/script.js
@@ -18,32 +18,41 @@ const $goodsList = [
 ];
 
 // Шаблон для добавления товара
-$(".cart-modal__list").append("<div class=\"cart-modal__goods mx-0 px-2\">\n" +
-    "                    <div class=\"grid-item-1\">\n" +
-    "                        <img class=\"cart-modal__goods--image\" src=\"img/products/furniture/14.png\" alt=\"14\">\n" +
-    "                    </div >\n" +
-    "                    <div class=\"grid-item-2\">\n" +
-    "                        <a href=\"#\" class=\"cart-modal__goods--title\">Sunbaby Magic Bear Chair</a>\n" +
-    "                    </div>\n" +
-    "                    <div class=\"grid-item-3\">\n" +
-    "                        <span class=\"cart-modal__goods--price\">$350.00</span>\n" +
-    "                        <span class=\"cart-modal__goods--discount-price\">$250.00</span>\n" +
-    "                    </div>\n" +
-    "                    <div class=\"grid-item-4\">\n" +
-    "                        <span class=\"cart-modal__goods--quantity-text\">Quantity:</span>\n" +
-    "                        <div class=\"cart-modal__goods--quantity-number\">2</div>\n" +
-    "                        <div class=\"cart-modal__goods--quantity-minus\">-</div>\n" +
-    "                        <div class=\"cart-modal__goods--quantity-plus\">+</div>\n" +
-    "                    </div>\n" +
-    "                    <div class=\"grid-item-5\">\n" +
-    "                        <div class=\"cart-modal__goods--sum-number\">$500.00</div>\n" +
-    "                    </div>\n" +
-    "                </div>");
+// $(".cart-modal__list").append("<div class=\"cart-modal__goods mx-0 px-2\">\n" +
+//     "                    <div class=\"grid-item-1\">\n" +
+//     "                        <img class=\"cart-modal__goods--image\" src=\"img/products/furniture/14.png\" alt=\"14\">\n" +
+//     "                    </div >\n" +
+//     "                    <div class=\"grid-item-2\">\n" +
+//     "                        <a href=\"#\" class=\"cart-modal__goods--title\">Sunbaby Magic Bear Chair</a>\n" +
+//     "                    </div>\n" +
+//     "                    <div class=\"grid-item-3\">\n" +
+//     "                        <span class=\"cart-modal__goods--price\">$350.00</span>\n" +
+//     "                        <span class=\"cart-modal__goods--discount-price\">$250.00</span>\n" +
+//     "                    </div>\n" +
+//     "                    <div class=\"grid-item-4\">\n" +
+//     "                        <span class=\"cart-modal__goods--quantity-text\">Quantity:</span>\n" +
+//     "                        <div class=\"cart-modal__goods--quantity-number\">2</div>\n" +
+//     "                        <div class=\"cart-modal__goods--quantity-minus\">-</div>\n" +
+//     "                        <div class=\"cart-modal__goods--quantity-plus\">+</div>\n" +
+//     "                    </div>\n" +
+//     "                    <div class=\"grid-item-5\">\n" +
+//     "                        <div class=\"cart-modal__goods--sum-number\">$500.00</div>\n" +
+//     "                    </div>\n" +
+//     "                </div>");
 
 // Конец
 
-// Вставка общей суммы в корзину
-$(".cart-modal__total-amount--number").text('$' + 1000);
+// Вставка общей суммы в корзине
+// $(".cart-modal__total-amount--number").text("$1000");
 
-// Вставка количества товаров в корзине
-$(".header__cart--quantity").text(2);
+// Вставка количества товаров в корзине (в главном меню)
+// $(".header__cart--quantity").text("2");
+
+// Ячейка с количеством одного наименования товара в корзине
+// $(".cart-modal__goods--quantity-number")
+
+// Кнопка "отнять -1 товар"
+// $(".cart-modal__goods--quantity-minus")
+
+// Кнопка "добавить +1 товар"
+// $(".cart-modal__goods--quantity-plus")
